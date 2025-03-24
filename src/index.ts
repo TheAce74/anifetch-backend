06c027b5adf2047e1f3cd6638ccf86c2f4414cff
Chisom Udonsi <udonsichisom02@gmail.com>
2025-03-23 22:22:46 -0400

    feat: update downloader

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions: CorsOptions = {
-  // use env variables next time
-  origin: ["http://localhost:5173", "https://anifetchbyace.vercel.app"],
+  origin: process.env.ORIGINS
+    ? process.env.ORIGINS.split(", ")
+    : ["http://localhost:5173"],
 };
 
 app.use(cors(corsOptions));
@@ -24,14 +25,7 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-    headless: true,
+    headless: process.env.NODE_ENV === "prod" ? true : false,
   });
 
   try {
@@ -41,7 +35,7 @@ app.get("/", async (req, res) => {
     };
 
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url, { waitUntil: "networkidle2" });
 
     setTimeout(async () => {
       const episodesMarkup = await page.content();
@@ -50,58 +44,44 @@ app.get("/", async (req, res) => {
         (link) => `https://animepahe.ru${link}`
       );
 
-      const episodeProviderLinks = [];
+      const episodeProviderLinks: string[] = [];
       for (let i = 0; i < fullLinks.length; i++) {
-        await page.goto(fullLinks[i]);
+        await page.goto(fullLinks[i], { waitUntil: "networkidle2" });
         const episodeMarkup = await page.content();
         episodeProviderLinks.push(
           extractEpisodeLinks(episodeMarkup, resolution)
         );
       }
-
       console.log(episodeProviderLinks);
 
-      const preDownloadLinks = [];
+      const preDownloadLinks: string[] = [];
       for (let i = 0; i < episodeProviderLinks.length; i++) {
-        await page.goto(episodeProviderLinks[i]);
+        const page = await browser.newPage();
+        await page.goto(episodeProviderLinks[i], { waitUntil: "networkidle2" });
         sleepSync(7000);
         const episodeProviderMarkup = await page.content();
-        preDownloadLinks.push(extractPreDownloadLinks(episodeProviderMarkup));
+        const downloadLink = extractPreDownloadLinks(episodeProviderMarkup);
+        preDownloadLinks.push(downloadLink);
+        await page.goto(downloadLink, { waitUntil: "networkidle2" });
+        page.on("response", async (response) => {
+          if (response.url().includes(".mp4")) {
+            preDownloadLinks.push(response.url());
+          }
+        });
+        await page.click('form button[type="submit"]');
       }
-
       console.log(preDownloadLinks);
-
-      //   for (let i = 0; i < preDownloadLinks.length; i++) {
-      //     await page.goto(preDownloadLinks[i]);
-      //     let downloadMarkup = await page.content();
-      //     const autoDownloadScript = `
-      //   <script>
-      //     window.onload = function() {
-      //       const downloadButton = document.querySelector('form button[type="submit"]');
-      //       if (downloadButton) {
-      //         downloadButton.click(); // Auto-click the download button
-      //       }
-      //     };
-      //   </script>
-      // `;
-      //     downloadMarkup = downloadMarkup.replace(
-      //       "</body>",
-      //       `${autoDownloadScript}</body>`
-      //     );
-      //     res.write(downloadMarkup);
-      //     sleepSync(5000);
-      //   }
 
       res.status(200).json({
         links: preDownloadLinks,
       });
+      await browser.close();
     }, 20000);
   } catch (error) {
     console.error(error);
     res.status(500).json({
       message: "Something went terribly wrong!",
     });
-  } finally {
     await browser.close();
   }
 });
